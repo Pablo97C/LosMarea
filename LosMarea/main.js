de941a32ee09ebc03a8fafa6eed022853b468b00
Pablo <pablo97chillon@gmail.com>
2021-06-22 16:12:39 +0200

    add files

---
+++ LosMarea/main.js
@@ -0,0 +1,77 @@
+// let navegador = document.querySelector("nav");
+// function progreso(){
+//     let progreso = document.documentElement.scrollTop;
+//     if(progreso > 0){
+//         navegador.className= "cambio";
+//     }
+//     console.log(progreso);
+// }
+// window.addEventListener("scroll", function(){
+//     progreso();
+// });
+
+
+const navegador = document.querySelector("nav");
+
+window.addEventListener('scroll',function(){
+    navegador.classList.toggle('cambio', window.scrollY > 0);
+});
+
+const tarjeta1 = document.querySelector("#tarjeta1");
+const subtarjeta1 = document.querySelector("#subtarjeta1");
+
+const tarjeta2 = document.querySelector('#tarjeta2');
+const subtarjeta2 = document.querySelector('#subtarjeta2')
+
+const tarjeta3 = document.querySelector('#tarjeta3');
+const subtarjeta3 = document.querySelector('#subtarjeta3')
+
+
+
+
+subtarjeta1.addEventListener('click',()=>{
+
+    tarjeta1.classList.toggle("desplegar");
+    subtarjeta1.classList.toggle("ocultar");
+
+    contenido.classList.toggle("ani");
+    // subtarjeta1.classList.toggle("ani");
+
+
+});
+
+tarjeta1.addEventListener('click',()=>{
+
+    tarjeta1.classList.toggle("desplegar");
+    subtarjeta1.classList.toggle("ocultar");
+});
+
+
+
+subtarjeta2.addEventListener('click',()=>{
+
+    tarjeta2.classList.toggle("desplegar");
+    subtarjeta2.classList.toggle("ocultar");
+});
+
+tarjeta2.addEventListener('click',()=>{
+
+    tarjeta2.classList.toggle("desplegar");
+    subtarjeta2.classList.toggle("ocultar");
+});
+
+
+
+subtarjeta3.addEventListener('click',()=>{
+
+    tarjeta3.classList.toggle("desplegar");
+    subtarjeta3.classList.toggle("ocultar");
+});
+
+tarjeta3.addEventListener('click',()=>{
+
+    tarjeta3.classList.toggle("desplegar");
+    subtarjeta3.classList.toggle("ocultar");
+});
+
+
