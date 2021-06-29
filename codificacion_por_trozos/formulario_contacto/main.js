b95edf15237d7dd8f5b0f24de2405989d19fbada
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2021-06-29 18:51:19 +0200

    manejando el evento FOCUS
    Hemos manejado el evento focus en el formulario de contacto para darle animación

--- codificacion_por_trozos/formulario_contacto/main.js
+++ codificacion_por_trozos/formulario_contacto/main.js
@@ -9,12 +9,17 @@
  * HTML DOM contains() Method: https://www.w3schools.com/jsref/met_node_contains.asp
  * classList: https://clubmate.fi/javascript-adding-and-removing-class-names-from-elements
  * HTML DOM Events: https://www.w3schools.com/jsref/dom_obj_event.asp
+ * The HTML DOM Element Object: https://www.w3schools.com/jsref/dom_obj_all.asp
+ * element: https://developer.mozilla.org/es/docs/Web/API/Element
+ * Element offsetParent Property: https://www.w3schools.com/jsref/prop_element_offsetparent.asp
+ * HTML DOM previousElementSibling Property: https://www.w3schools.com/jsref/prop_element_previouselementsibling.asp
+ * 
  */
 
 // Carga del DOM completada
 window.addEventListener('load', () => {
 
-   animacionInput2();
+    animacionInputFocus1();
 });
 
 
@@ -41,8 +46,8 @@ function animacionInput() {
 
                 const label = divHijo.querySelector('label');
                 //const input = divHijo.querySelector('input'); 
-                label.classList.remove('animacion-input'); 
-            } 
+                label.classList.remove('animacion-input');
+            }
         }
 
         /////////////////////////////////////////////////////////////////////////////
@@ -64,38 +69,101 @@ function animacionInput() {
     });
 }
 
-function animacionInput2(){
+function animacionInput2() {
     const formularioContacto = document.querySelector('#idFormularioContacto');
-    formularioContacto.addEventListener('click', (evento)=>{
-          //console.log(evento.target.nodeName);
+    formularioContacto.addEventListener('click', (evento) => {
+        //console.log(evento.target.nodeName);
         const arrayDivDeFormulario = formularioContacto.children;
 
+        let divTarget = evento.target.offsetParent;
+
+        // recorre todos los div hijo del formulario
         for (let i = 0; i < arrayDivDeFormulario.length; i++) {
             let divHijo = arrayDivDeFormulario[i];
-            
-            
+
+
             // console.log(divHijo);
             //console.log("classList: ", divHijo.classList); // devuelve array de clases
             //console.log("className: ", divHijo.className); // devuelve string
 
+            // Si el divHijo es el campo que nos interesa animar
             if (divHijo.classList.contains("campoInput")) {
-                const label = divHijo.querySelector('label');
 
+                // label dentro del divHijo
+                let label = divHijo.querySelector('label');
 
-                if (label.classList.contains('animacion-input')){ 
-                    label.classList.remove('animacion-input');
-                    // label.classList.add('animacion-input-off');
+                // Si el div que he clickado es igual al divHijo que estamos recorriendo actualmente
+                if (divTarget === divHijo) {
+
+                    // Si el div que hemos pulsado no contenia la animación, se la añadimos.
+                    if (!label.classList.contains('animacion-input')) {
+                        label.classList.add('animacion-input');
+                    }
                 }
-            } 
+                // Para el resto de divHijo que no han sido pulsados, tendremos que borrarle la clase que los anima.
+                else {
+                    label.classList.remove('animacion-input');
+                    label.classList.add('animacion-input-off');
+                }
+            }
         }
-        
-            
-            let div = evento.target.offsetParent;
-            let label = div.querySelector('label');
-            // console.log(label);
-            label.classList.add('animacion-input');
-
     })
 
 }
 
+function animacionInputFocus1() {
+    // formulario de contacto
+    let formularioContacto = document.querySelector("#idFormularioContacto");
+
+    // añadimos evento de focus-in
+    formularioContacto.addEventListener("focusin", (evento) => {
+        // elemento que ha ganado el foco
+        let elementoTarget = evento.target;
+
+        // Si el elemento que ha sido pulsado es un input y tiene la clase 'inputFormulario'
+        if (elementoTarget.nodeName === 'INPUT' && elementoTarget.classList.contains('inputFormulario')) {
+
+            // No haría falta, pero asignamos el elemento a una nueva variable para llamarlo de forma más intuitiva
+            let inputFormulario = elementoTarget;
+
+            // obtenemos la etiqueta label que es hermana previa del input
+            let labelFormulario = inputFormulario.previousElementSibling;
+
+            // Si label no contiene la clase de la animación, se la añadimos
+            if (!labelFormulario.classList.contains('animacion-input')) {
+                labelFormulario.classList.add('animacion-input');
+            }
+        }
+
+
+    });
+
+
+    // añadimos evento de focus-out
+    formularioContacto.addEventListener("focusout", (evento) => {
+        // elemento que ha perdido el foco
+        let elementoTarget = evento.target;
+
+        // Si el elemento que ha sido pulsado es un input y tiene la clase 'inputFormulario'
+        if (elementoTarget.nodeName === 'INPUT' && elementoTarget.classList.contains('inputFormulario')) {
+
+            // No haría falta, pero asignamos el elemento a una nueva variable para llamarlo de forma más intuitiva
+            let inputFormulario = elementoTarget;
+            // obtenemos la etiqueta label que es hermana previa del input
+            let labelFormulario = inputFormulario.previousElementSibling;
+
+            // Si label contiene la clase de la animación, se la quitamos
+            if (labelFormulario.classList.contains('animacion-input')) {
+                labelFormulario.classList.remove('animacion-input');
+                labelFormulario.classList.add('animacion-input-off');
+            }
+        }
+
+
+
+
+
+    });
+
+
+}
