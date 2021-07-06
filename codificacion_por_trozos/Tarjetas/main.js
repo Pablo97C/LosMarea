8b1216c36e87a9172acb48834dd0c51c64b1a204
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2021-07-06 18:52:59 +0200

    eventos click
    se ha modificado los eventos click de las tarjetas

--- codificacion_por_trozos/Tarjetas/main.js
+++ codificacion_por_trozos/Tarjetas/main.js
@@ -1,60 +1,75 @@
+/**
+ * WEBGRAFIA
+ * =========
+ * https://www.w3schools.com/jsref/jsref_foreach.asp
+ */
+
 window.addEventListener('load', () => {
-    // tarjetas();
+    tarjetas();
 
 });
 
 // Cambiar apariencia entre tag1 y tag
 
 function tarjetas() {
-    const tarjeta1 = document.querySelector("#tarjeta1");
-    const subtarjeta1 = document.querySelector("#subtarjeta1");
+    const tarjetasNoticias = document.querySelectorAll(".tarjeta-noticias");
+    // const subtarjeta1 = document.querySelector("#subtarjeta1");
 
-    const tarjeta2 = document.querySelector('#tarjeta2');
-    const subtarjeta2 = document.querySelector('#subtarjeta2');
+    // const tarjeta2 = document.querySelector('#tarjeta2');
+    // const subtarjeta2 = document.querySelector('#subtarjeta2');
 
-    const tarjeta3 = document.querySelector('#tarjeta3');
-    const subtarjeta3 = document.querySelector('#subtarjeta3');
-
-
+    // const tarjeta3 = document.querySelector('#tarjeta3');
+    // const subtarjeta3 = document.querySelector('#subtarjeta3');
 
 
-    subtarjeta1.addEventListener('click', () => {
-
-        tarjeta1.classList.toggle("desplegar");
-        subtarjeta1.classList.toggle("ocultar");
+    tarjetasNoticias.forEach((tarjeta, indice, array) => {
+        tarjeta.addEventListener('click', (evento) => {
+            const subtarjetaFoto = tarjeta.querySelector('.subtarjeta-foto');
+            const subtarjetaConcierto = tarjeta.querySelector('.subtarjeta-concierto');
+            const subtarjetaDetalle = tarjeta.querySelector('.subtarjeta-detalle');
+            subtarjetaFoto.classList.toggle("ocultar");
+            subtarjetaConcierto.classList.toggle("hover-translate");
+            subtarjetaDetalle.classList.toggle("hover-translate");
+        });
     });
 
-    tarjeta1.addEventListener('click', () => {
+    // subtarjeta1.addEventListener('click', () => {
 
-        tarjeta1.classList.toggle("desplegar");
-        subtarjeta1.classList.toggle("ocultar");
-    });
+    //     tarjeta1.classList.toggle("desplegar");
+    //     subtarjeta1.classList.toggle("ocultar");
+    // });
 
+    // tarjeta1.addEventListener('click', () => {
 
-
-    subtarjeta2.addEventListener('click', () => {
-
-        tarjeta2.classList.toggle("desplegar");
-        subtarjeta2.classList.toggle("ocultar");
-    });
-
-    tarjeta2.addEventListener('click', () => {
-
-        tarjeta2.classList.toggle("desplegar");
-        subtarjeta2.classList.toggle("ocultar");
-    });
+    //     tarjeta1.classList.toggle("desplegar");
+    //     subtarjeta1.classList.toggle("ocultar");
+    // });
 
 
 
-    subtarjeta3.addEventListener('click', () => {
+    // subtarjeta2.addEventListener('click', () => {
 
-        tarjeta3.classList.toggle("desplegar");
-        subtarjeta3.classList.toggle("ocultar");
-    });
+    //     tarjeta2.classList.toggle("desplegar");
+    //     subtarjeta2.classList.toggle("ocultar");
+    // });
 
-    tarjeta3.addEventListener('click', () => {
+    // tarjeta2.addEventListener('click', () => {
 
-        tarjeta3.classList.toggle("desplegar");
-        subtarjeta3.classList.toggle("ocultar");
-    });
+    //     tarjeta2.classList.toggle("desplegar");
+    //     subtarjeta2.classList.toggle("ocultar");
+    // });
+
+
+
+    // subtarjeta3.addEventListener('click', () => {
+
+    //     tarjeta3.classList.toggle("desplegar");
+    //     subtarjeta3.classList.toggle("ocultar");
+    // });
+
+    // tarjeta3.addEventListener('click', () => {
+
+    //     tarjeta3.classList.toggle("desplegar");
+    //     subtarjeta3.classList.toggle("ocultar");
+    // });
 }
